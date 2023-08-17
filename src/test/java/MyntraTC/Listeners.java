9e72e4a2c74e83aec3166a155595e977f62f034c
Admin <Admin@DESKTOP-VAJVVUP>
2023-08-17 14:36:47 +0530

    ss add

--- src/test/java/MyntraTC/Listeners.java
+++ src/test/java/MyntraTC/Listeners.java
@@ -2,6 +2,7 @@ package MyntraTC;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -13,12 +14,20 @@ import org.testng.ITestResult;
 
 public class Listeners extends TestBaseClass implements ITestListener    {
 	
+	Date date = new Date();
+	String date1 = date.toString();
+	String date2 = date1.replaceAll(" ", "_");
+	
 	@Override  
 	public void onTestFailure(ITestResult result) {
+		
+		
+		
 		 String name = result.getMethod().getMethodName();
 		TakesScreenshot ts = (TakesScreenshot) driver;
 		File source = ts.getScreenshotAs(OutputType.FILE);
-		File dest= new File("./ScreenShots/"+name+".png");
+		File dest= new File(".//ScreenShots//"+name+".png");
+		
 		try {
 			FileUtils.copyFile(source, dest);
 		} catch (IOException e) {
